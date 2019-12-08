07113aaa256d6500de16e902229657686cdd6377
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2019-12-08 23:18:50 +0000

    updated

---
+++ helpers.R
@@ -0,0 +1,16 @@
+library(jsonlite)
+library(httr)
+
+json_to_csv <- function(x, filepath = "latest/mtcars.csv") {
+  df <- fromJSON(x)
+  write.csv(df, file = paste0("data/", filepath))
+}
+
+ukhp_get <- function(x, frequency = "monthly", classification = "nuts1", vintage = "latest") {
+  endpoint <- "https://lancs-macro.github.io/uk-house-prices"
+  query <- paste(endpoint, vintage, frequency, paste0(classification, ".json"), sep = "/")
+  request <- GET(query)
+  stop_for_status(request)
+  parse_json(content(request), simplifyVector = TRUE)
+} 
+
