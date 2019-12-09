2fc97fadf5408d6493270f735323530e0abea8d9
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2019-12-09 15:16:37 +0000

    preliminary working version

--- helpers.R
+++ helpers.R
@@ -1,10 +1,15 @@
 library(jsonlite)
 library(httr)
 
-json_to_csv <- function(x, filepath = "latest/mtcars.csv") {
+json_to_csv <- function(x, filepath = "latest/nuts1.csv") {
   df <- fromJSON(x)
   write.csv(df, file = paste0("data/", filepath))
 }
+
+# json_to_csv(x = "docs/latest/monthly/nuts1.json")
+# json_to_csv(x = "docs/latest/monthly/nuts2.json", "latest/nuts2.csv")
+# json_to_csv(x = "docs/latest/monthly/nuts3.json", "latest/nuts3.csv")
+
 
 ukhp_get <- function(x, frequency = "monthly", classification = "nuts1", release = "latest") {
   endpoint <- "https://lancs-macro.github.io/uk-house-prices"
